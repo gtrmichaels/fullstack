061f9ea6702d6122904b9f2b99f5eaa9985c3c25
gtrmichaels <gtrmichaels@gmail.com>
2025-03-26 21:02:35 +0300

    update fizzbuzz

--- fizzbuzz/fizzbuzz.js
+++ fizzbuzz/fizzbuzz.js
@@ -1,3 +1,15 @@
-function fizzbuzz (){
-    
+let answer parseInt(prompt("Please enter a number you would like to fizzbuzz upto: "));
+
+for (let i = 1; i <= answer; i++){
+    if (i % 3 === 0 && i % 5 === 0){
+        console.log("fizzbuzz");
+    }
+    else if (i % 3 === 0) {
+        console.log("fizz");
+    }
+    else if (i % 5 === 0){
+        console.log("buzz");
+    }
+    else
+    console.log(i);
 }
